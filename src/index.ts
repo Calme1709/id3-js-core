965822b1bae8b9c734b6a2135eec3a2bacea0096
Calme1709 <callumlaw1709@outlook.com>
2020-01-30 10:08:44 +1300

    Remove test code

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,13 @@
-import TextInformationFrame from './frames/textInformationFrame';
+import Decoder from "./decoder";
+
+import { Buffer } from "buffer";
 export default class ID3JS {
 	public static write(){
-		const frame = new TextInformationFrame("TEXT", "Test");
 
-		const encodedFrame = frame.encode({ textEncoding: "latin1", ID3Version: 4 });
-		
-		console.log(encodedFrame.toString("hex").match(/../g)?.join(" "));
-		console.log(frame.toString());
-
-		console.log(new TextInformationFrame(encodedFrame, 4).toString());
 	}
 
-	public static read(){
-		
+	public static read(data: Buffer){
+		return Decoder.decode(data);
 	}
 
 	public static remove(){
@@ -23,5 +18,3 @@ export default class ID3JS {
 
 	}
 }
-
-ID3JS.write();
