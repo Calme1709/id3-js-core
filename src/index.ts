453494427b8f3a68b721cae297b390be9c00b104
Calme1709 <callumlaw1709@outlook.com>
2020-02-05 22:58:25 +1300

    Export all frames to the user of the library

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ import Decoder from "@decoder";
 import { Buffer } from "buffer";
 import { Frame } from "@frames";
 import { IUserDefinedEncodingOptions } from '@encoder/encodingOptions';
+
+export * from "./frames";
+
 export default class ID3JS {
 	public static write(data: Buffer, frames: Frame[], encodingOptions?: IUserDefinedEncodingOptions){
 		return Buffer.concat([
