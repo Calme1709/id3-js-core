456dea4697673bcf95016b309dc4f113a87f26b0
Calme1709 <callumlaw1709@outlook.com>
2020-03-05 14:26:43 +1300

    Make bufferFromNumbers write all numbers with the smallest amount of size that it can unless an amount of bytes per number is specified

--- src/utils/bufferFromNumbers.ts
+++ src/utils/bufferFromNumbers.ts
@@ -6,14 +6,20 @@ import { Buffer } from "buffer";
  * @param size - The size of the buffer to create (defaults to the minimum size to store the passed number)
  */
 export default (numbers: number[], bytesPerNumber?: number) => {
-	const bytesPerNum = typeof bytesPerNumber === "number" ?
-		bytesPerNumber :
-		Math.ceil(Math.log2(Math.max(...numbers) + 1) / 8);
+	const bufferSize = typeof bytesPerNumber === "number" ?
+		bytesPerNumber * numbers.length :
+		numbers.reduce((acc, cur) => acc + Math.ceil(Math.log2(cur + 1) / 8));
 
-	const buffer = Buffer.alloc(bytesPerNum * numbers.length, 0);
+	const buffer = Buffer.alloc(bufferSize, 0);
 
-	numbers.forEach((num, index) => {
-		buffer.writeIntBE(num, index * bytesPerNum, bytesPerNum);
+	let offset = 0;
+
+	numbers.forEach(num => {
+		const byteLength = typeof bytesPerNumber === "number" ? bytesPerNumber : Math.ceil(Math.log2(num + 1) / 8);
+
+		buffer.writeIntBE(num, offset, byteLength);
+
+		offset += byteLength;
 	});
 
 	return buffer;
